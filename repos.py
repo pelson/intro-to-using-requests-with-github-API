3bd613141a4cbbb695e2c18e547a53fc632923b8
Phil Elson <pelson.pub@gmail.com>
2017-08-07 13:57:11 +0100

    Added a Jupyter notebook as a tutorial.

--- repos.py
+++ repos.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from pprint import pprint
 
 import requests
 
@@ -8,14 +9,22 @@ def api_token():
     if not token_file.exists():
         raise IOError('Please create a token at github.com, and save it in {}'.format(token_file))
 
-    token = token_file.read_text()
+    token = token_file.read_text().strip()
     return token
 
 
 def main():
     token = api_token()
-    print(token)
-    resp = requests.get('')
+    headers = {'Authorization': 'token {}'.format(token)}
+
+    API_URL = 'https://api.github.com'
+
+    target = '/user/repos'
+    resp = requests.get('{}{}'.format(API_URL, target),
+                        headers=headers)
+    content = resp.json()
+    pprint(content[:3])
+
 
 if __name__ == '__main__':
     main()
